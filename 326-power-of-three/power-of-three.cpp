cd9a772cfbe77dbc7f9d138402a37ea629616631
nandini-mp <nandinimprakash.06@gmail.com>
2025-05-16 19:44:15 +0530

    Time: 6 ms (27.63%) | Memory: 9 MB (45.57%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n<=0)
+        if (n==1)
+            return true;
+        if (n<=0 || n%3!=0)
             return false;
-        while (n!=0)
-        {
-            if (n==1 || n==3)
-                return true;
-            if (n%3!=0)
-                return false;
+        while (n%3==0)
             n/=3;
-        }
-        return true;
+        return n==1;
     }
 };
