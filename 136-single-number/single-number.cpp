3ac9b199ca1a3241ecb664d8886c6f131198eebc
nandini-mp <nandinimprakash.06@gmail.com>
2025-05-25 12:16:14 +0530

    Time: 11 ms (16.28%) | Memory: 23.8 MB (13.55%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> numbers;
+        for (int i=0;i<nums.size();i++)
+            numbers[nums[i]]++;
         int i;
         for (i=0;i<nums.size();i++)
         {
-            if (count(nums.begin(),nums.end(),nums[i])==2)
+            if (numbers[nums[i]]==2)
                 continue;
             return nums[i];
         }
