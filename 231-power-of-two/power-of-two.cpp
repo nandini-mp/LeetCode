f55188f85205376bd287e94a02fd93895092b977
nandini-mp <nandinimprakash.06@gmail.com>
2025-05-27 18:21:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (18.47%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n<=0)
+            return false;
+        if ((int)log2(n)==log2(n))
+            return true;
+        return false;
+    }
+};
