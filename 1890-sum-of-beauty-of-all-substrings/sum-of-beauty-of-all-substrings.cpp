59387aae80f0d2d8655eafbab848795198e6691c
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-04 11:03:16 +0530

    Time: 110 ms (70.48%) | Memory: 11.2 MB (73.09%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.size(),result=0;
+        for (int i=0;i<n;i++)
+        {
+            vector<int> freq(26,0);
+            for (int j=i;j<n;j++)
+            {
+                freq[s[j]-'a']++;
+                int maxFreq=0,minFreq=INT_MAX;
+                for (int k=0;k<26;k++)
+                {
+                    if (freq[k]>0)
+                    {
+                        maxFreq=max(maxFreq,freq[k]);
+                        minFreq=min(minFreq,freq[k]);
+                    }
+                }
+                result+=maxFreq-minFreq;
+            }
+        }
+        return result;
+    }
+};
