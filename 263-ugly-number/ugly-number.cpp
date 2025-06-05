b25a7c628c1dab398a3360257f9c9003221a2967
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-05 23:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (10.44%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n<=0) return false;
+        if (n==1) return true;
+        vector<int> primes={2,3,5};
+        for (int p:primes)
+            while (n%p==0)
+                n/=p;
+        return n==1;
+    }
+};
