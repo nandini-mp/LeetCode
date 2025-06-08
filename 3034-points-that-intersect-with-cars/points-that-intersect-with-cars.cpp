e8b3668ec4860913c908b08bfaca180515e3db7a
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-08 16:22:47 +0530

    Time: 2 ms (51.90%) | Memory: 28.8 MB (94.78%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    int numberOfPoints(vector<vector<int>>& nums) {
-        unordered_set<int> covered;
-        for (auto& car:nums)
-            for (int i=car[0];i<=car[1];i++)
-                covered.insert(i);
-        return covered.size();
+    int numberOfPoints(vector<vector<int>>& v) {
+        sort(v.begin(), v.end()); //lowest to highest start point
+        int ans = 0, k = 0; //ans is total unique points so far, k furthest point
+        for (auto &i : v) { //each car's rangle - [start,end]
+            if (i[1] > k) { //means this interval extends past what's already covered
+                ans += i[1] - max(i[0] - 1, k); //i[0]-1 because start is included, i[1] is the end point which is new (going past)
+            }
+            k = max(i[1], k); //update new
+        }
+        return ans;
     }
 };
