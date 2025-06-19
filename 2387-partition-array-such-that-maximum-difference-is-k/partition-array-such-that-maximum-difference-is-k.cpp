e7f64c6317ef80a49d767a93ccee92447b71d6fd
nandini-mp <nandinimprakash.06@gmail.com>
2025-06-19 10:09:13 +0530

    Time: 30 ms (83.92%) | Memory: 86.8 MB (71.13%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.cpp
@@ -13,3 +13,28 @@ public:
         return count;
     }
 };
+
+/*class Solution {
+public:
+    static int partitionArray(vector<int>& nums, int k) {
+        if (nums.size() == 1)
+            return 1; // edge case
+
+        int freq[100001] = {0}, xMax = -1;
+        for (int x : nums) {
+            freq[x]++;
+            xMax = max(xMax, x);
+        }
+
+        int partitions = 0;
+        for (int x = 0; x <= xMax; x+=k+1) { // start from 0, step k+1
+            // skip x when freq[x]=0 but x++
+            while (x <= xMax && freq[x] == 0)
+                x++;
+            if (x > xMax)
+                break;
+        }
+        return partitions;
+    }
+};
+*/
