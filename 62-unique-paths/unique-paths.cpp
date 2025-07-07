58c3312a0aa9a8ec6791e58aea11691affd57e2f
nandini-mp <nandinimprakash.06@gmail.com>
2025-07-07 11:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.00%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<int> prev(n,0);
+        for (int i=0;i<m;i++)
+        {
+            vector<int> temp(n,0);
+            for (int j=0;j<n;j++)
+            {
+                if (i==0 && j==0) temp[j]=1;
+                else
+                {
+                    int up=0,left=0;
+                    if (i>0) up=prev[j];
+                    if (j>0) left=temp[j-1];
+                    temp[j]=up+left;
+                }
+            }
+            prev=temp;
+        }
+            
+        return prev[n-1];
+    }
+};
+
+/*
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,0));
         for (int i=0;i<m;i++)
             for (int j=0;j<n;j++)
@@ -17,3 +43,4 @@ public:
         return dp[m-1][n-1];
     }
 };
+*/
