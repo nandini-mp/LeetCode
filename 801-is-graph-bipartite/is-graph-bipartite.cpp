6fa4e9c5ee955ad1e62df53569a767fd78417412
nandini-mp <nandinimprakash.06@gmail.com>
2025-07-18 13:42:37 +0530

    Time: 3 ms (16.19%) | Memory: 17.6 MB (38.24%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+    bool bfs(int node,vector<vector<int>>& graph,vector<int>& colours)
+    {
+        queue<int> q;
+        q.push(node);
+        while (!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            for (int neighbour:graph[node])
+            {
+                if (colours[neighbour]==-1)
+                {
+                    colours[neighbour]=1-colours[node];
+                    q.push(neighbour);
+                }
+                else if (colours[neighbour]==colours[node])
+                    return false;                   
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> colours(n,-1);
+        for (int i=0;i<n;i++)
+        {
+            if (colours[i]==-1)
+            {
+                colours[i]=0;
+                if (!bfs(i,graph,colours)) return false;
+            } 
+        }
+        return true;
+    }
+};
+
+/*
+class Solution {
+public:
     bool dfs(int node,int colour,vector<vector<int>>& graph,vector<int>& colours)
     {
         colours[node]=colour;
@@ -26,3 +66,4 @@ public:
         return true;
     }
 };
+*/
